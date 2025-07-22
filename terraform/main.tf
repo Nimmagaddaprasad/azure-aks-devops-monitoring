615550934e9f653b0fcc1061236af5667bbe88d2
Durga Prasad Nimmagadda <nimmagadda.prasad92@gmail.com>
2025-07-22 09:50:40 +0000

    Fix: updated AKS nodepool VM size to available SKU

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size    = "Standard_DS2_v2"
+    vm_size    = "Standard_D2als_v6"
   }
 
   identity {
